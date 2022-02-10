317ac1840f1da4681847fb48ace44f6a050417d1
rleone72 <58292146+rleone72@users.noreply.github.com>
2022-02-10 12:13:23 +0100

    Delete sufasicnadm directory

--- sufasicnadm/secret/icp4a-root-ca/icp4a-root-ca.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Add BIT root CA certificate for the "ldap-ssl-secret"
-kubectl create secret generic icp4a-root-ca --from-file=tls.crt=./icp4a-root-ca.pem
--- sufasicnadm/secret/ldap-ssl-secret/ldap_certs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Add BIT root CA certificate for the "ldap-ssl-secret"
-kubectl create secret generic ldap-ssl-secret --from-file=tls.crt=./ldap_db00048a.pem
