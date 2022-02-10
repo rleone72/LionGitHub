cdf5275a0e34ffaedc5d1aeacef1c2af9041105b
rleone72 <58292146+rleone72@users.noreply.github.com>
2022-02-10 12:13:14 +0100

    Delete sufas directory

--- sufas/CPE/create_master_properties_folders.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cpepod=`oc get pods | grep cpe | awk '{print $1}'`
-oc rsh $cpepod
-mkdir -p configDropins/overrides/SUFAS/SUFAS/
-exit
--- sufas/ICN/copy_master_properties.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-icnpod=`oc get pods | grep navi | awk '{print $1}'`
-echo Copying master.properties to pod $icnpod
-oc cp master.properties $icnpod:/opt/ibm/wlp/usr/servers/defaultServer/configDropins/overrides/SUFAS/SUFAS/
