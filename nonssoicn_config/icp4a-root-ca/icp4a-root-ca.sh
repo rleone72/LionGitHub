088725d7a405949f21dbb4268cedc204aa650ef6
rleone72 <58292146+rleone72@users.noreply.github.com>
2022-02-10 12:12:51 +0100

    Delete nonssoicn_config directory

--- nonssoicn_config/icp4a-root-ca/icp4a-root-ca.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Add BIT root CA certificate for the "ldap-ssl-secret"
-kubectl create secret generic icp4a-root-ca --from-file=tls.crt=/home/cp4ba-inst/nonssoicn_config/icp4a-root-ca/icp4a-root-ca.pem
